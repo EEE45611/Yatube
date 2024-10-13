57ee306f9792a4f12b0a1e0873a70cb3d0a7c8b5
Your Name <you@example.com>
2024-10-13 20:53:46 +0500

    falee image

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,4 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.index),
-    path('IceCream', views.index2),
-    path('IceCream/<id>', views.index3),
-    path('<id>', views.index4),
 ]
