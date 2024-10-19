07614fd3418897326e0347fce8db5768b598c614
Your Name <you@example.com>
2024-10-19 20:05:46 +0500

    new pages!!

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,4 +2,8 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.index),
+    path('about', views.about),
+    path('posts', views.posts),
+    path('register', views.register),
+    path('login', views.login),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 def index(request):
     return render(request, 'posts/index.html')
+def about(request):
+    return render(request, 'posts/about.html')
+def posts(request):
+    return render(request, 'posts/posts.html')
+def register(request):
+    return render(request, 'posts/register.html')
+def login(request):
+    return render(request, 'posts/login.html')
