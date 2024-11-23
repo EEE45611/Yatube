d2af6a0d217ad463cbeeb1127bff284c04223d51
Your Name <you@example.com>
2024-11-23 20:14:42 +0500

    omg11!!

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class comment(models.Model):
+    text=models.TextField()
+    author=models.CharField(max_length=20)
+    date=models.CharField(max_length=20)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,21 @@
+from posts.models import comment
 from django.shortcuts import render
+
 def index(request):
     return render(request, 'posts/index.html')
 def about(request):
     return render(request, 'posts/about.html')
+comments=[
+    {'author':'Иван Петрович','date':'2.11.24;15:24','text':'все проверил, спасибо'},
+    {'author': 'Иван Иванов', 'date': '01-12-2023', 'text': 'Отличная статья!'},
+    {'author': 'Петр Петров 2', 'date': '02-12-2023', 'text': 'Спасибо за полезную информацию.'},
+    {'author': 'Анна Сидорова', 'date': '03-12-2023', 'text': 'Очень понравилось! Буду ждать новых статей.'}
+]
+context = {'comments': comments}
 def posts(request):
-    return render(request, 'posts/posts.html')
+    comments=comment.objects.all()
+    print(comments[0].author)
+    return render(request, 'posts/posts.html',context={'comments': comments})
 def register(request):
     return render(request, 'posts/register.html')
 def login(request):
